fec0cda99f5ee2c01655f2e39e4ce0df706f58d7
Choyoungjun <cyj070513@gmail.com>
2023-09-17 00:06:32 +0900

    📑 :: LoginViewController 생성

---
+++ SVAP_iOS/SVAP_iOS/Sources/Auth/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  SVAP_iOS
+//
+//  Created by 조영준 on 2023/09/17.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
