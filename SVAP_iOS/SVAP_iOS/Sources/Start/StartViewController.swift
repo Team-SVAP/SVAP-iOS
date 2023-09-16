24ee21080a8c466b95f7dd21e7b60c903d0da8cd
Choyoungjun <cyj070513@gmail.com>
2023-09-17 00:45:50 +0900

    📑 :: StartViewController 생성

---
+++ SVAP_iOS/SVAP_iOS/Sources/Start/StartViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+import SnapKit
+import Then
+
+class StartViewController: BaseVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func configureUI() {
+        
+    }
+    override func setConstraints() {
+        <#code#>
+    }
+}
