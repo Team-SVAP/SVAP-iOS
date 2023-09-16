43c279c58319662463a04c1f6004346ecb74d35b
Choyoungjun <cyj070513@gmail.com>
2023-09-17 02:01:49 +0900

    📑 :: IdViewController 생성

---
+++ SVAP_iOS/SVAP_iOS/Sources/Auth/Signup/IdViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+import SnapKit
+import Then
+
+class IdViewController: BaseVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func configureUI() {
+        
+    }
+    override func setConstraints() {
+        <#code#>
+    }
+}
