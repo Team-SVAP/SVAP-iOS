929cf968d6e2adce4a3cc570844405d422d3e3e6
cyj513 <cyj070513@gmail.com>
2023-09-25 23:15:05 +0900

    📑 :: PetitionCell 생성

---
+++ SVAP_iOS/SVAP_iOS/Sources/Main/Cell/Main/MainCell.swift
@@ -0,0 +1,34 @@
+import UIKit
+import SnapKit
+import Then
+
+class MainCell: UICollectionViewCell {
+    
+    static let id = "Cell1"
+    let cellImage = UIImageView(image: UIImage(named: "mainCell"))
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        configureUI()
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setup() {
+        self.backgroundColor = UIColor(named: "main-8")
+    }
+    func configureUI() {
+        [
+            cellImage
+        ].forEach({ contentView.addSubview($0) })
+        
+    }
+    func setupConstraints() {
+        cellImage.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
---
+++ SVAP_iOS/SVAP_iOS/Sources/Main/Cell/Petition/PetitionCell.swift
@@ -0,0 +1,16 @@
+import UIKit
+import SnapKit
+import Then
+
+class PetitionCell: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
