cb687ff7371845e1eebf482ad7e87171b9906333
cyj513 <cyj070513@gmail.com>
2023-09-26 20:15:55 +0900

    📑 :: TextField Component 추가

--- SVAP_iOS/SVAP_iOS/Sources/Component/PetitionButton.swift
+++ SVAP_iOS/SVAP_iOS/Sources/Component/PetitionButton.swift
@@ -17,10 +17,10 @@ class PetitionButton: UIButton {
         self.layer.shadowOpacity = 1
         self.layer.cornerRadius = 24
         self.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMaxYCorner]
-        self.layer.shadowColor = UIColor(named: "gray-300")?.cgColor
+        self.layer.shadowColor = UIColor(named: "gray-200")?.cgColor
         self.layer.shadowOffset = CGSize.zero
         self.layer.shadowOffset = CGSize(width: 0, height: 4)
-        self.layer.shadowRadius = 4
+        self.layer.shadowRadius = 2
     }
     convenience init(type: UIButton.ButtonType, title: String, image: UIImage) {
         self.init(type: type)
---
+++ SVAP_iOS/SVAP_iOS/Sources/Component/SearchTextField.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class SearchTextField: UITextField {
+    
+    private var placeholderText: String = ""
+
+    init(
+        placeholder: String
+    ) {
+        super.init(frame: .zero)
+        self.autocapitalizationType = .none
+        self.autocorrectionType = .no
+        self.placeholderText = placeholder
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 12
+        self.textColor = UIColor(named: "gray-800")
+        self.font = UIFont(name: "IBMPlexSansKR-Regular", size: 16)
+        self.layer.borderColor = UIColor(named: "main-3")?.cgColor
+        self.layer.borderWidth = 1
+        let leftSpacerView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        let rightSpacerView = UIView(frame: CGRect(x: 0, y: 0, width: 45, height: 0))
+        self.leftView = leftSpacerView
+        self.rightView = rightSpacerView
+        self.leftViewMode = .always
+        self.rightViewMode = .always
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        placeholderSetting()
+    }
+    
+    func placeholderSetting() {
+        self.attributedPlaceholder = NSAttributedString(
+            string: placeholderText,
+            attributes: [
+                NSAttributedString.Key.foregroundColor: UIColor(named: "gray-300")!,
+                NSAttributedString.Key.font: UIFont(name: "IBMPlexSansKR-Regular", size: 12)!
+            ]
+        )
+    }
+
+}
