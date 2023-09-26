c08cbbf4f1454c4c9361c0b6a71e940c3cd02b62
cyj513 <cyj070513@gmail.com>
2023-09-27 08:50:21 +0900

    정리 커밋

--- SVAP_iOS/SVAP_iOS/Sources/Main/Petition/CreatePetitionViewController.swift
+++ SVAP_iOS/SVAP_iOS/Sources/Main/Petition/CreatePetitionViewController.swift
@@ -3,33 +3,55 @@ import UIKit
 class CreatePetitionViewController: BaseVC {
 
     var isClick = false
-    let leftButton = UIButton(type: .system)
-    let rightButton = UIButton(type: .system)
+    private let leftButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(named: "leftArrow"), for: .normal)
+        $0.tintColor = UIColor(named: "gray-700")
+    }
+    private let rightButton = UIButton(type: .system).then {
+        $0.setTitle("완료", for: .normal)
+        $0.setTitleColor(UIColor(named: "gray-600"), for: .normal)
+        $0.titleLabel?.font = UIFont(name: "IBMPlexSansKR-Medium", size: 16)
+    }
     private let titleLabel = UILabel().then {
         $0.text = "*제목"
         $0.textColor = UIColor(named: "gray-700")
         $0.font = UIFont(name: "IBMPlexSansKR-Medium", size: 14)
     }
-    private let enterTitleTextField = CustomTextField(placeholder: "제목을 입력하세요.", isSecure: false).then {
+    private let enterTitleTextField = SearchTextField(placeholder: "제목을 입력하세요.").then {
         $0.layer.borderColor = UIColor(named: "gray-400")?.cgColor
+        $0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 8
     }
     private let typeLabel = UILabel().then {
         $0.text = "*종류"
         $0.textColor = UIColor(named: "gray-700")
         $0.font = UIFont(name: "IBMPlexSansKR-Medium", size: 14)
     }
+    private let typeView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.borderColor = UIColor(named: "gray-400")?.cgColor
+        $0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 8
+    }
     private let placeLabel = UILabel().then {
         $0.text = "*위치태그"
         $0.textColor = UIColor(named: "gray-700")
         $0.font = UIFont(name: "IBMPlexSansKR-Medium", size: 14)
+    }
+    private let placeTextField = SearchTextField(placeholder: "").then {
+        $0.layer.borderColor = UIColor(named: "gray-400")?.cgColor
+        $0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 8
     }
     private let contentLabel = UILabel().then {
         $0.text = "*내용"
         $0.textColor = UIColor(named: "gray-700")
         $0.font = UIFont(name: "IBMPlexSansKR-Medium", size: 14)
     }
-    private let enterContentTextField = CustomTextField(placeholder: "내용을 입력하세요.", isSecure: false).then {
+    private let enterContentTextField = SearchTextField(placeholder: "내용을 입력하세요.").then {
         $0.layer.borderColor = UIColor(named: "gray-400")?.cgColor
+        $0.layer.borderWidth = 0.5
+        $0.layer.cornerRadius = 8
     }
     
     override func viewDidLoad() {
@@ -42,7 +64,13 @@ class CreatePetitionViewController: BaseVC {
         
         [
             titleLabel,
-            enterTitleTextField
+            enterTitleTextField,
+            typeLabel,
+            typeView,
+            placeLabel,
+            placeTextField,
+            contentLabel,
+            enterContentTextField
         ].forEach({ view.addSubview($0) })
     }
     override func setupConstraints() {
@@ -57,6 +85,36 @@ class CreatePetitionViewController: BaseVC {
             $0.left.right.equalToSuperview().inset(20)
             $0.height.equalTo(40)
         }
+        typeLabel.snp.makeConstraints {
+            $0.top.equalTo(enterTitleTextField.snp.bottom).offset(24)
+            $0.left.equalToSuperview().inset(20)
+        }
+        typeView.snp.makeConstraints {
+            $0.top.equalTo(typeLabel.snp.bottom).offset(8)
+            $0.left.equalToSuperview().inset(20)
+            $0.width.equalTo(165)
+            $0.height.equalTo(40)
+        }
+        placeLabel.snp.makeConstraints {
+            $0.top.equalTo(enterTitleTextField.snp.bottom).offset(24)
+            $0.right.equalToSuperview().inset(126)
+        }
+        placeTextField.snp.makeConstraints {
+            $0.top.equalTo(placeLabel.snp.bottom).offset(8)
+            $0.right.equalToSuperview().inset(20)
+            $0.width.equalTo(165)
+            $0.height.equalTo(40)
+        }
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(typeView.snp.bottom).offset(24)
+            $0.left.equalToSuperview().inset(20)
+        }
+        enterContentTextField.snp.makeConstraints {
+            $0.top.equalTo(contentLabel.snp.bottom).offset(8)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.width.equalTo(350)
+            $0.height.equalTo(220)
+        }
     }
     private func navigationBarSetting() {
         navigationItem.hidesBackButton = true
@@ -67,25 +125,20 @@ class CreatePetitionViewController: BaseVC {
         title.textAlignment = .center
         navigationItem.titleView = title
         
-        leftButton.setImage(UIImage(named: "leftArrow"), for: .normal)
-        leftButton.tintColor = UIColor(named: "gray-700")
         leftButton.addTarget(self, action: #selector(clickLeftBarButton), for: .touchUpInside)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
     
-        rightButton.setTitle("완료", for: .normal)
-        rightButton.setTitleColor(UIColor(named: "gray-600"), for: .normal)
-        rightButton.titleLabel?.font = UIFont(name: "IBMPlexSansKR-Medium", size: 16)
         rightButton.addTarget(self, action: #selector(clickRightBarButton), for: .touchUpInside)
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightButton)
     }
-    @objc func clickLeftBarButton() {
+    @objc private func clickLeftBarButton() {
         self.navigationController?.popViewController(animated: true)
     }
-    @objc func clickRightBarButton() {
+    @objc private func clickRightBarButton() {
         self.navigationController?.popViewController(animated: true)
         //청원등록 서버통신
     }
-    @objc func clickButton() {
+    @objc private func clickButton() {
         isClick.toggle()
         if isClick {
 //            self.navigationController?.present(CustomMenu(), animated: true)
