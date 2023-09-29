adffc66c2aff2368b62ecb3e250d3bb912f7de59
cyj513 <cyj070513@gmail.com>
2023-09-29 23:39:05 +0900

    📑 :: 청원 목록 뷰 작성

--- SVAP_iOS/SVAP_iOS/Sources/Main/Petition/PetitionViewController.swift
+++ SVAP_iOS/SVAP_iOS/Sources/Main/Petition/PetitionViewController.swift
@@ -14,14 +14,18 @@ class PetitionViewController: BaseVC {
     private let line = UIView().then {
         $0.backgroundColor = UIColor(named: "gray-200")
     }
-    
     private let tableView = UITableView().then {
-        $0.backgroundColor = .gray
+        $0.backgroundColor = .white
+        $0.register(PetitionCell.self, forCellReuseIdentifier: "CellID")
+        $0.rowHeight = 80
+        $0.separatorStyle = .none
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationBarSetting()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     private func navigationBarSetting() {
         let title = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
@@ -65,8 +69,34 @@ class PetitionViewController: BaseVC {
             $0.height.equalTo(0.5)
         }
         tableView.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(175)
-            $0.left.right.bottom.equalToSuperview()
+            $0.top.equalTo(line.snp.bottom).offset(24.25)
+            $0.bottom.equalToSuperview()
+            $0.left.right.equalToSuperview().inset(20)
         }
+    }
+}
+
+extension PetitionViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 30
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath) as! PetitionCell
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 12.0 // 각 섹션 사이의 간격을 조절합니다.
+    }
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView()
+        footerView.backgroundColor = .clear // 섹션 사이의 간격 부분을 투명하게 만듭니다.
+        return footerView
     }
 }
