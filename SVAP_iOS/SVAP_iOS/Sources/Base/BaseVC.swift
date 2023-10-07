e872250ed0cd5a97b5f42d1da55ddb45eb6264d5
cyj513 <cyj070513@gmail.com>
2023-10-08 02:03:51 +0900

    ⚡️ :: 페이지를 밀어서 이전페이지로 이동하는 기능 추가

--- SVAP_iOS/SVAP_iOS/Sources/Base/BaseVC.swift
+++ SVAP_iOS/SVAP_iOS/Sources/Base/BaseVC.swift
@@ -3,11 +3,12 @@ import SnapKit
 import Then
 import Moya
 
-class BaseVC: UIViewController {
+class BaseVC: UIViewController, UIGestureRecognizerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = self
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
