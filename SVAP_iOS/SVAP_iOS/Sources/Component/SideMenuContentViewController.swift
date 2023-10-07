5f9ee05222ca0a4454b1da66bf8d48ddac6a9bd2
cyj513 <cyj070513@gmail.com>
2023-10-08 01:53:59 +0900

    📑 :: sideMenuContentViewController 생성

---
+++ SVAP_iOS/SVAP_iOS/Sources/Component/SideMenuContentViewController.swift
@@ -0,0 +1,93 @@
+import UIKit
+import SnapKit
+import Then
+
+class SideMenuContentViewController: UIViewController {
+    
+    private let backgroundView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 20
+        $0.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+        $0.clipsToBounds = true
+    }
+    private let settingButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(named: "settingIcon"), for: .normal)
+        $0.tintColor = UIColor(named: "gray-700")
+    }
+    private let closeButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(named: "closeIcon"), for: .normal)
+        $0.tintColor = UIColor(named: "gray-700")
+        $0.addTarget(self, action: #selector(clickCloseButton), for: .touchUpInside)
+    }
+    private let userNameLabel = UILabel().then {
+        $0.text = "조영준"
+        $0.textColor = UIColor(named: "gray-800")
+        $0.font = UIFont(name: "IBMPlexSansKR-Medium", size: 16)
+    }
+    private let lineView = UIView().then {
+        $0.backgroundColor = UIColor(named: "gray-200")
+    }
+    private let myPetitionButton = UIButton(type: .system).then {
+        $0.setTitle("내가 쓴 청원보기", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "IBMPlexSansKR-Regular", size: 16)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+        navigationController?.navigationBar.isHidden = true
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        configureUI()
+        setupConstraints()
+    }
+    
+    private func configureUI() {
+        view.addSubview(backgroundView)
+        [
+            settingButton,
+            closeButton,
+            userNameLabel,
+            lineView,
+            myPetitionButton
+        ].forEach({ backgroundView.addSubview($0) })
+    }
+    private func setupConstraints() {
+        
+        backgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(54)
+            $0.bottom.right.equalToSuperview()
+            $0.width.equalTo(200)
+        }
+        settingButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(24)
+            $0.left.equalToSuperview().inset(20)
+            $0.width.height.equalTo(20)
+        }
+        closeButton.snp.makeConstraints {
+            $0.top.right.equalToSuperview().inset(20)
+            $0.width.height.equalTo(24)
+        }
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(68)
+            $0.left.equalToSuperview().inset(20)
+        }
+        lineView.snp.makeConstraints {
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(24)
+            $0.left.equalToSuperview().inset(22)
+            $0.right.equalToSuperview().inset(18)
+            $0.height.equalTo(1.5)
+        }
+        myPetitionButton.snp.makeConstraints {
+            $0.top.equalTo(lineView.snp.bottom).offset(24)
+            $0.left.equalToSuperview().inset(22)
+        }
+    }
+    
+    @objc private func clickCloseButton() {
+        self.dismiss(animated: true)
+    }
+    
+}
