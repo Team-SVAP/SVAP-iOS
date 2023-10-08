b5cd71bb55f1d76923d6d4a68d5a464b54bc1bc7
cyj513 <cyj070513@gmail.com>
2023-10-09 01:18:23 +0900

    ⚡️ :: refreshToken 추가

--- SVAP_iOS/SVAP_iOS/Sources/Service/Header/Header.swift
+++ SVAP_iOS/SVAP_iOS/Sources/Service/Header/Header.swift
@@ -4,34 +4,44 @@ import SwiftKeychainWrapper
 //리프레시 토큰 로직 혼자 짜보자 ㅅㅂ
 struct Token {
     static var localAccessToken: String?
-//    static var refrash: String? {
-//
-//    }
+    static var serverAccessToken: String?
+    
+    static var refreshToken: String? {
+        get {
+            serverAccessToken = KeychainWrapper.standard.string(forKey: "refresh_token")
+            return serverAccessToken
+        }
+        set(newtoken) {
+            KeychainWrapper.standard.set(newtoken ?? "nil", forKey: "refresh_token")
+            serverAccessToken = newtoken
+        }
+    }
     static var accessToken: String? {
         get {
-            localAccessToken = KeychainWrapper.standard.string(forKey: "access-token")
+            localAccessToken = KeychainWrapper.standard.string(forKey: "accessToken")
             //오류터지면 언더바로 바꾸자...
             return localAccessToken
         }
         set(newToken) {
-            KeychainWrapper.standard.set(newToken ?? "nil", forKey: "access-token")
+            KeychainWrapper.standard.set(newToken ?? "nil", forKey: "accessToken")
             localAccessToken = newToken
         }
     }
     static func removeToken() {
+        self.refreshToken = nil
         self.accessToken = nil
     }
 }
 
 enum Header {
-    case refrashToken, accessToken, tokenIsEmpty
+    case refreshToken, accessToken, tokenIsEmpty
     func header() -> [String: String]? {
         guard let token = Token.accessToken,
               token != "nil" else {
             return ["Content-Type": "application/json"]
         }
         switch self {
-            case .refrashToken:
+            case .refreshToken:
                 return ["Authorization": "Bearer " + token, "Content-Type": "application/json"]
             case .accessToken:
                 return ["Authorization": "Bearer " + token, "Content-Type": "application/json"]
