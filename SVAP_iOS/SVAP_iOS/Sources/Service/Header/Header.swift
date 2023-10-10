a90be20979492923ff31f537612adce9a471f2db
cyj513 <cyj070513@gmail.com>
2023-10-10 23:39:12 +0900

    ♻️ :: 토큰 방식 수정

--- SVAP_iOS/SVAP_iOS/Sources/Service/Header/Header.swift
+++ SVAP_iOS/SVAP_iOS/Sources/Service/Header/Header.swift
@@ -1,25 +1,24 @@
 import Foundation
 import SwiftKeychainWrapper
 
-//리프레시 토큰 로직 혼자 짜보자 ㅅㅂ
 struct Token {
     static var localAccessToken: String?
     static var serverAccessToken: String?
     
     static var refreshToken: String? {
         get {
-            serverAccessToken = KeychainWrapper.standard.string(forKey: "refresh_token")
+            serverAccessToken = KeychainWrapper.standard.string(forKey: "refreshToken")
             return serverAccessToken
         }
         set(newtoken) {
-            KeychainWrapper.standard.set(newtoken ?? "nil", forKey: "refresh_token")
+            KeychainWrapper.standard.set(newtoken ?? "nil", forKey: "refreshToken")
             serverAccessToken = newtoken
         }
     }
     static var accessToken: String? {
         get {
             localAccessToken = KeychainWrapper.standard.string(forKey: "accessToken")
-            //오류터지면 언더바로 바꾸자...
+
             return localAccessToken
         }
         set(newToken) {
@@ -44,7 +43,7 @@ enum Header {
             case .refreshToken:
                 return ["Authorization": "Bearer " + token, "Content-Type": "application/json"]
             case .accessToken:
-                return ["Authorization": "Bearer " + token, "Content-Type": "application/json"]
+                return ["Authorization": token, "Content-Type": "application/json"]
             case .tokenIsEmpty:
                 return ["Content-Type": "application/json"]
         }
