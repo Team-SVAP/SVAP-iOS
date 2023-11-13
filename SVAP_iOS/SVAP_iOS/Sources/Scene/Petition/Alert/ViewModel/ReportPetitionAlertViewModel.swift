6a8b134ea7f99834411edf412f3c28455d12cc9c
Choyoungjun <cyj070513@gmail.com>
2023-11-13 23:31:02 +0900

    📑 :: ReportPetitionAlertViewModel 생성

---
+++ SVAP_iOS/SVAP_iOS/Sources/Scene/Petition/Alert/ViewModel/ReportPetitionAlertViewModel.swift
@@ -0,0 +1,21 @@
+//import Foundation
+//import RxSwift
+//import RxCocoa
+//
+//class ReportPetitionAlertViewModel: ViewModelType {
+//    
+//    private let disposeBag = DisposeBag()
+//    
+//    struct Input {
+//        
+//    }
+//    
+//    struct Output {
+//        
+//    }
+//    
+//    func transform(_ input: Input) -> Output {
+//
+//    }
+//    
+//}
