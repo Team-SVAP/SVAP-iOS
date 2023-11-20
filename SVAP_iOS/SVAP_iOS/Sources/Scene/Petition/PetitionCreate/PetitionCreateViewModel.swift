7514daca4ee1199f291c48cfa2867e5f49f43eee
Choyoungjun <cyj070513@gmail.com>
2023-11-20 23:16:25 +0900

    ⚡️ :: PetitionCreateViewModel 로직 작성

--- SVAP_iOS/SVAP_iOS/Sources/Scene/Petition/PetitionCreate/PetitionCreateViewModel.swift
+++ SVAP_iOS/SVAP_iOS/Sources/Scene/Petition/PetitionCreate/PetitionCreateViewModel.swift
@@ -1,8 +1,58 @@
-//
-//  CreatePetitionViewModel.swift
-//  SVAP_iOS
-//
-//  Created by 조영준 on 10/26/23.
-//
-
 import Foundation
+import RxSwift
+import RxCocoa
+
+class PetitionCreateViewModel: ViewModelType {
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let title: Driver<String>
+        let types: String
+        let location: Driver<String>
+        let content: Driver<String>
+        let images: [Data]
+        let imageURL: [String]?
+        let doneTap: Signal<Void>
+        let successSignal: Signal<Void>
+    }
+    
+    struct Output {
+        let imageResult: PublishRelay<ImageModel>
+        let petitionResult: PublishRelay<Bool>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        
+        let api = PetitionService()
+        let info = Driver.combineLatest(input.title, input.location, input.content)
+        let imageResult = PublishRelay<ImageModel>()
+        let petitionResult = PublishRelay<Bool>()
+        
+        input.doneTap.asObservable()
+            .flatMap { api.sendImage(input.images) }
+            .subscribe(onNext: { data, res in
+                switch res {
+                    case .createOk:
+                        imageResult.accept(data!)
+                    default:
+                        return
+                }
+            }).disposed(by: disposeBag)
+        
+        input.successSignal.asObservable()
+            .withLatestFrom(info)
+            .flatMap { title, location, content in
+                api.createPetition(title, content, location, input.types, input.imageURL) }
+            .subscribe(onNext: { res in
+                switch res {
+                    case .createOk:
+                        petitionResult.accept(true)
+                    default:
+                        petitionResult.accept(false)
+                }
+            }).disposed(by: disposeBag)
+        return Output(imageResult: imageResult, petitionResult: petitionResult)
+    }
+    
+}
