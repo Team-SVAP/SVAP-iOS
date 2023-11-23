9c1aff500f899371569217dfabcd635a319ea6c0
Choyoungjun <cyj070513@gmail.com>
2023-11-23 10:14:40 +0900

    ⚡️ :: Extension 추가

--- SVAP/SVAP/Sources/Extension/CALayerExtension.swift
+++ SVAP/SVAP/Sources/Extension/CALayerExtension.swift
@@ -3,6 +3,10 @@ import UIKit
 
 extension CALayer {
     
+    func borderColor(_ color: UIColor) {
+        self.borderColor = color.cgColor
+    }
+    
     func border(_ color: UIColor, _ width: CGFloat) {
         self.borderColor = color.cgColor
         self.borderWidth = width
--- SVAP/SVAP/Sources/Extension/UITextFieldExtension.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import UIKit
-
-extension UITextField {
-    
-    func borderColor(_ color: UIColor) {
-        self.layer.borderColor = color.cgColor
-    }
-}
