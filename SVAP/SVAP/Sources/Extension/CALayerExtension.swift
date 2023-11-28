862bbd908a2928568a64c2dd414d4388e07929f1
Choyoungjun <cyj070513@gmail.com>
2023-11-28 20:26:08 +0900

    ⚡️ :: Extension 추가

--- SVAP/SVAP/Sources/Extension/CALayerExtension.swift
+++ SVAP/SVAP/Sources/Extension/CALayerExtension.swift
@@ -11,4 +11,31 @@ extension CALayer {
         self.borderColor = color.cgColor
         self.borderWidth = width
     }
+    
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        self.cornerRadius = cornerRadius
+        self.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+    
+    func shadow(
+        color: UIColor,
+        alpha: Float,
+        x: CGFloat,
+        y: CGFloat,
+        blur: CGFloat,
+        spread: CGFloat
+    ) {
+        masksToBounds = false
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / UIScreen.main.scale
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let rect = bounds.insetBy(dx: -spread, dy: -spread)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+    
 }
