f1fdca73afe433ff4c7fba25192ae58576560d74
Choyoungjun <cyj070513@gmail.com>
2023-11-29 19:54:22 +0900

    📑 :: PetitionMenuViewController 생성

---
+++ SVAP/SVAP/Sources/Scene/Petition/Alert/View/PetitionMenuViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class PetitionMenuViewController: BaseVC {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+    override func configureUI() {
+        super.configureUI()
+    }
+    override func setupConstraints() {
+        super.setupConstraints()
+    }
+    override func bind() {
+        super.bind()
+    }
+    override func subscribe() {
+        super.subscribe()
+    }
+
+}
