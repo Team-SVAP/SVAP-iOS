70cb116ca9c39bb8581fb563b004ad7850f4656c
Choyoungjun <cyj070513@gmail.com>
2023-12-01 15:53:42 +0900

    ⚡️ :: 마이페이지 퍼블리싱 및 서버통신 완료

--- SVAP/SVAP/Sources/Scene/MyPage/MyPageViewController.swift
+++ SVAP/SVAP/Sources/Scene/MyPage/MyPageViewController.swift
@@ -1,13 +1,13 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import Moya
 
 class MyPageViewController: BaseVC {
     
     private let disposeBag = DisposeBag()
     private let viewModel = MyPageViewModel()
     private let viewAppear = PublishRelay<Void>()
+    private let membershipCancelSiganl = PublishRelay<Void>()
     
     private let topPaddingView = UIView().then {
         $0.backgroundColor = .white
@@ -23,6 +23,24 @@ class MyPageViewController: BaseVC {
         $0.font = UIFont(name: "IBMPlexSansKR-Medium", size: 24)
         $0.numberOfLines = 2
     }
+    private let line1 = UIView().then {
+        $0.backgroundColor = UIColor(named: "gray-200")
+    }
+    private let viewUserPetitionButton = LabelButton(type: .system, title: "내가 쓴 청원 보기", titleColor: UIColor(named: "gray-800")!)
+    private let line2 = UIView().then {
+        $0.backgroundColor = UIColor(named: "gray-200")
+    }
+    private let modifyUserInfo = LabelButton(type: .system, title: "내 정보 수정", titleColor: UIColor(named: "gray-800")!)
+    private let line3 = UIView().then {
+        $0.backgroundColor = UIColor(named: "gray-200")
+    }
+    private let buttonStackView = UIStackView().then {
+        $0.spacing = 24
+        $0.axis = .horizontal
+        $0.backgroundColor = .clear
+    }
+    private let logoutButton = LabelButton(type: .system, title: "로그아웃", titleColor: UIColor(named: "gray-800")!)
+    private let membershipCancelButton = LabelButton(type: .system, title: "회원 탈퇴", titleColor: UIColor(named: "gray-800")!)
     private let bottomPaddingView = UIView().then {
         $0.backgroundColor = .white
     }
@@ -40,9 +58,15 @@ class MyPageViewController: BaseVC {
         [
             topPaddingView,
             userNameLabel,
+            line1,
+            viewUserPetitionButton,
+            line2,
+            modifyUserInfo,
+            line3,
+            buttonStackView,
             bottomPaddingView
         ].forEach({ view.addSubview($0) })
-        
+        [logoutButton, membershipCancelButton].forEach({ buttonStackView.addArrangedSubview($0) })
         topPaddingView.addSubview(navigationTitleLabel)
         
     }
@@ -60,6 +84,33 @@ class MyPageViewController: BaseVC {
             $0.top.equalToSuperview().inset(115)
             $0.left.equalToSuperview().inset(20)
         }
+        line1.snp.makeConstraints {
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(20)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.height.equalTo(1)
+        }
+        viewUserPetitionButton.snp.makeConstraints {
+            $0.top.equalTo(line1.snp.bottom).offset(20)
+            $0.left.equalToSuperview().inset(20)
+        }
+        line2.snp.makeConstraints {
+            $0.top.equalTo(viewUserPetitionButton.snp.bottom).offset(20)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.height.equalTo(1)
+        }
+        modifyUserInfo.snp.makeConstraints {
+            $0.top.equalTo(line2.snp.bottom).offset(20)
+            $0.left.equalToSuperview().inset(20)
+        }
+        line3.snp.makeConstraints {
+            $0.top.equalTo(modifyUserInfo.snp.bottom).offset(20)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.height.equalTo(1)
+        }
+        buttonStackView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(line3.snp.bottom).offset(20)
+        }
         bottomPaddingView.snp.makeConstraints {
             $0.left.right.equalToSuperview()
             $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(82)
@@ -70,22 +121,67 @@ class MyPageViewController: BaseVC {
     
     override func bind() {
         let input = MyPageViewModel.Input(
-            viewAppear: viewAppear.asSignal(onErrorJustReturn: ())
+            viewAppear: viewAppear.asSignal(onErrorJustReturn: ()),
+            deleteTap: membershipCancelSiganl.asSignal(onErrorJustReturn: ())
         )
         let output = viewModel.transform(input)
         
         output.userName.asObservable()
             .subscribe(onNext: { data in
-                if data.userName != "" {
-                    self.userNameLabel.text = "\(data.userName ?? "")님,\n안녕하세요!"
-                } else {
-                    self.userNameLabel.text = "User not found"
-                }
+                self.userNameLabel.text = "\(data.userName ?? "User not found")님,\n안녕하세요!"
             })
             .disposed(by: disposeBag)
+        
+        output.deleteResult.asObservable()
+            .subscribe(onNext: { bool in
+                if bool {
+                    print("Success")
+                } else {
+                    print("Fail")
+                }
+            }).disposed(by: disposeBag)
     }
     
     override func subscribe() {
+        super.subscribe()
+        
+        viewUserPetitionButton.rx.tap
+            .subscribe(onNext: {
+                let vc = UserPetitionViewController()
+                vc.hidesBottomBarWhenPushed = true
+                self.pushViewController(vc)
+            }).disposed(by: disposeBag)
+        
+        logoutButton.rx.tap
+            .subscribe(onNext: {
+                let modal = MyPageAlert(
+                    questionLabelText: "로그아웃 하시겠습니까?",
+                    explainLabelText: "다시 로그인 해야 합니다.",
+                    completion: {
+                        Token.removeToken()
+                        let vc = LoginViewController()
+                        vc.hidesBottomBarWhenPushed = true
+                        self.pushViewController(vc)
+                    })
+                modal.modalPresentationStyle = .overFullScreen
+                modal.modalTransitionStyle = .crossDissolve
+                self.present(modal, animated: true)
+            }).disposed(by: disposeBag)
+        membershipCancelButton.rx.tap
+            .subscribe(onNext: {
+                let modal = MyPageAlert(
+                    questionLabelText: "회원탈퇴 하시겠습니까?",
+                    explainLabelText: "계정이 삭제됩니다.",
+                    completion: {
+                        self.membershipCancelSiganl.accept(())
+                        let vc = OnboardingViewController()
+                        vc.hidesBottomBarWhenPushed = true
+                        self.pushViewController(vc)
+                    })
+                modal.modalPresentationStyle = .overFullScreen
+                modal.modalTransitionStyle = .crossDissolve
+                self.present(modal, animated: true)
+            }).disposed(by: disposeBag)
         
     }
     
--- SVAP/SVAP/Sources/Scene/MyPage/MyPageViewModel.swift
+++ SVAP/SVAP/Sources/Scene/MyPage/MyPageViewModel.swift
@@ -8,14 +8,17 @@ class MyPageViewModel: ViewModelType {
     
     struct Input {
         let viewAppear: Signal<Void>
+        let deleteTap: Signal<Void>
     }
     struct Output {
         let userName: PublishRelay<UserInfoModel>
+        let deleteResult: PublishRelay<Bool>
     }
     func transform(_ input: Input) -> Output {
         
         let api = AuthService()
         let userInfo = PublishRelay<UserInfoModel>()
+        let deleteResult = PublishRelay<Bool>()
         
         input.viewAppear.asObservable()
             .flatMap{ api.loadUserInfo() }
@@ -29,6 +32,18 @@ class MyPageViewModel: ViewModelType {
                         return
                 }
             }).disposed(by: disposeBag)
-        return Output(userName: userInfo)
+        
+        input.deleteTap.asObservable()
+            .flatMap { api.userWithdrawal() }
+            .subscribe(onNext: { res in
+                switch res {
+                    case .ok:
+                        deleteResult.accept(true)
+                    default:
+                        deleteResult.accept(false)
+                }
+            }).disposed(by: disposeBag)
+        
+        return Output(userName: userInfo, deleteResult: deleteResult)
     }
 }
