0283fb0f73df1515fa7ce172c39770e9744528fd
Choyoungjun <cyj070513@gmail.com>
2023-12-14 15:18:44 +0900

    📑 :: 정보 수정 페이지 생성

---
+++ SVAP/SVAP/Sources/Scene/MyPage/View/PasswordChangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PasswordChangeViewController.swift
+//  SVAP
+//
+//  Created by 조영준 on 12/12/23.
+//
+
+import UIKit
+
+class PasswordChangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SVAP/SVAP/Sources/Scene/MyPage/View/UserInfoModifyViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UserInfoModifyViewController.swift
+//  SVAP
+//
+//  Created by 조영준 on 12/12/23.
+//
+
+import Foundation
